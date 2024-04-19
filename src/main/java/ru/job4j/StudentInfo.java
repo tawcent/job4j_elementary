62c7a4b41b877591820147f6cd22e754e3c7127f
tawcent <rusx745@gmail.com>
2024-04-19 21:48:29 +0400

    3.3. Фиксация изменений на github [#243564]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Ruslan Dautov");
+        System.out.println("22.08.1992");
+    }
+}
