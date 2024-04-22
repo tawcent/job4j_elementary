febb794ca4be4c63d05d88f66dd4b99ce1b7d7f7
tawcent <rusx745@gmail.com>
2024-04-22 21:29:26 +0400

    3.3. Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/calculator/MathFunction.java
+++ src/main/java/ru/job4j/calculator/MathFunction.java
@@ -20,7 +20,7 @@ public class MathFunction {
         int result1 = MathFunction.func1(3);
         int result2 = MathFunction.func2(5);
         int result3 = MathFunction.func3(10);
-        int total = result1 + result2+result3;
+        int total = result1 + result2 + result3;
         System.out.println(total);
     }
 }
