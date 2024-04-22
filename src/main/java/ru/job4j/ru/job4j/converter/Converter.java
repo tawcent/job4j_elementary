b99306b36af2167b75383b181809dbf76c6fe9ea
tawcent <rusx745@gmail.com>
2024-04-22 21:31:32 +0400

    3.3. Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/ru/job4j/converter/Converter.java
@@ -13,7 +13,7 @@ public class Converter {
 
     }
 
-    public static void main(String [] args) {
+    public static void main(String[] args) {
         float euro = Converter.rubleToEuro(140);
         float dollar = Converter.rubleToDollar(180);
         System.out.println("140 rubles are " + euro + " euro");
