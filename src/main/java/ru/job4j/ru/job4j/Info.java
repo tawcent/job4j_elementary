deb35e3558b160836cffc19e8b82b422074df464
tawcent <rusx745@gmail.com>
2024-04-22 19:14:26 +0400

    3.3. Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/ru/job4j/Info.java
+++ src/main/java/ru/job4j/ru/job4j/Info.java
@@ -2,6 +2,6 @@ package ru.job4j.ru.job4j;
 
 public class Info {
     public static void main(String[] args) {
-        System.out.println ("21.04.2024");
+        System.out.println("21.04.2024");
     }
 }
