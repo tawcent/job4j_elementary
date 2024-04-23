c037dfc1d2359765941c75f542f9d23aa5469af1
tawcent <rusx745@gmail.com>
2024-04-23 18:31:57 +0400

    8. Стороны прямоугольника. [#137735]

--- src/main/java/ru/job4j/condition/TriangleArea.java
+++ src/main/java/ru/job4j/condition/TriangleArea.java
@@ -6,8 +6,8 @@ public class TriangleArea {
         double s = Math.sqrt(p);
         return s;
     }
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         double result = TriangleArea.area(2, 2, 2);
         System.out.println("area (2, 2, 2) = " + result);
-        }
     }
+}
