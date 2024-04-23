c8cf5e067b3add994bc0c9437d9409fa0871e4ff
tawcent <rusx745@gmail.com>
2024-04-23 19:08:03 +0400

    8. Стороны прямоугольника. [#137735]

---
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -0,0 +1,13 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Jawa!";
+        System.out.println(idea);
+        idea += " But a im a newbie. ";
+        System.out.println(idea);
+        int year = 31;
+        idea += year;
+        System.out.println(idea + " years old");
+    }
+}
