b6d5f9a13eba39e30d42da6d42bf31f5b9cd1809
tawcent <rusx745@gmail.com>
2024-04-25 20:27:49 +0400

    8. Стороны прямоугольника. [#137735]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -8,16 +8,26 @@ public class Converter {
     }
 
     public static float rubleToDollar(float value) {
-        float result = value / 60;
-        return result;
+        float resultdol = value / 60;
+        return resultdol;
 
     }
 
     public static void main(String[] args) {
         float euro = Converter.rubleToEuro(140);
-        float dollar = Converter.rubleToDollar(180);
-        System.out.println("140 rubles are " + euro + " euro");
-        System.out.println("180 dollars are " + dollar + " dollars");
+        float dollar = Converter.rubleToDollar(240);
+        float input = 140;
+        float expected = 2;
+        float output = Converter.rubleToEuro(input);
+        boolean passed = expected == output; /* test euro */
+        float input1 = 240;
+        float expected1 = 4;
+        float output1 = Converter.rubleToDollar(input1);
+        boolean passed1 = expected1 == output1; /* test dollar */
+        System.out.println("140 rubles are " + euro + " euro. Test result : " + passed);
+        System.out.println("180 dollars are " + dollar + " dollars. Test result : " + passed1);
+
 
     }
+
 }
