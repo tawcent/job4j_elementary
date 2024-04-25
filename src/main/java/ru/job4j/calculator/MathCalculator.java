b33a882835012feca3af94cf913a38a596217576
tawcent <rusx745@gmail.com>
2024-04-25 21:58:26 +0400

    8. Стороны прямоугольника. [#137735]

--- src/main/java/ru/job4j/calculator/MathCalculator.java
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -3,16 +3,20 @@ package ru.job4j.calculator;
 import static ru.job4j.math.MathFunction.*;
 
 public class MathCalculator {
+
     public static double sumAndMultiply(double first, double second) {
         return sum(first, second) + multiply(first, second);
+
     }
 
     public static double sumDivAndDiff(double first, double second) {
         return diff(first, second) + div(first, second);
+
     }
 
     public static double allSum(double first, double second) {
         return sum(first, second) + multiply(first, second) + diff(first, second) + div(first, second);
+
     }
 
     public static void main(String[] args) {
