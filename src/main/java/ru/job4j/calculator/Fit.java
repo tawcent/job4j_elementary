b806cd86f244883e3093599e1c69bc4ac0505a22
tawcent <rusx745@gmail.com>
2024-04-27 21:16:31 +0400

    8. Стороны прямоугольника. [#137735]

--- src/main/java/ru/job4j/calculator/Fit.java
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -2,11 +2,17 @@ package ru.job4j.calculator;
 
 public class Fit {
     public static double manWeight(short heightMan) {
+        if (heightMan <= 100) {
+            throw new IllegalArgumentException("Рост мужчины должен быть больше 100 см");
+        }
         double result = (heightMan - 100) * 1.15;
         return result;
     }
 
     public static double womanWeight(short heightWoman) {
+        if (heightWoman <= 110) {
+            throw new IllegalArgumentException("Рост женщины должен быть больше 110 см");
+        }
         double result = (heightWoman - 110) * 1.15;
         return result;
     }
