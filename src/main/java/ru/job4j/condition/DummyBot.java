b681799cc835b9e9aea46f36f4dd776f390e9831
tawcent <rusx745@gmail.com>
2024-04-30 20:33:00 +0400

    8. Стороны прямоугольника. [#137735]

--- src/main/java/ru/job4j/condition/DummyBot.java
+++ src/main/java/ru/job4j/condition/DummyBot.java
@@ -2,11 +2,11 @@ package ru.job4j.condition;
 
 public class DummyBot {
     public static String answer(String question) {
-        String result = "I don`t know. Please, ask another question.";
+        String result = "I don't know. Please, ask another question.";
         if ("Hi, Bot.".equals(question)) {
             result = "Hi, SmartAss.";
         } else if ("Bye.".equals(question)) {
-            result = "See you later";
+            result = "See you later.";
         }
         return result;
     }
