8f126357c4a65cee00d20c6d1fb17b815701e9c2
tawcent <rusx745@gmail.com>
2024-05-01 19:30:52 +0400

    8. Стороны прямоугольника. [#137735]

--- src/main/java/ru/job4j/condition/Club.java
+++ src/main/java/ru/job4j/condition/Club.java
@@ -2,14 +2,14 @@ package ru.job4j.condition;
 
 public class Club {
     public static void permission(boolean hasMoney, boolean beFriend) {
-        if(hasMoney || beFriend) {
+        if (hasMoney || beFriend) {
             System.out.println("I can go to the club");
         } else {
             System.out.println("I can't");
         }
     }
 
-    public static void main(String [] args) {
+    public static void main(String[] args) {
         Club.permission(true, true);
         Club.permission(true, false);
         Club.permission(false, true);
