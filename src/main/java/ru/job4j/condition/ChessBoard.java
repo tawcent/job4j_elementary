a2490cb790fad4895da49196b70485c34a84d0cb
tawcent <rusx745@gmail.com>
2024-05-03 19:30:47 +0400

    8. Стороны прямоугольника. [#137735]

---
+++ src/main/java/ru/job4j/condition/ChessBoard.java
@@ -0,0 +1,21 @@
+package ru.job4j.condition;
+
+public class ChessBoard {
+    public static int way(int x1, int y1, int x2, int y2) {
+            int result = 0;
+            if (isValid(x1) && isValid(x2)
+                    && isValid(y1) && isValid(y2)) {
+                 if (Math.abs(x1 - x2) == Math.abs(y1 - y2)) {
+                    result = Math.abs(x1 - x2);
+                }
+            }
+            return result;
+        }
+
+        private static boolean isValid(int coordinate) {
+            return coordinate >= 0 && coordinate <= 7;
+        }
+
+    }
+
+
