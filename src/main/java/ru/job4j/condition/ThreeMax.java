879f5fefa146813ea29ef1e43f113dab290d0f22
tawcent <rusx745@gmail.com>
2024-05-05 16:42:47 +0400

    8. Стороны прямоугольника. [#137735]

--- src/main/java/ru/job4j/condition/ThreeMax.java
+++ src/main/java/ru/job4j/condition/ThreeMax.java
@@ -5,11 +5,11 @@ public class ThreeMax {
         int result = first;
         if (first >= second && first >= third) {
             result = first;
-
-        } else if (second >= first && second >= third) {
+        }
+        if (second >= first && second >= third) {
             result = second;
-
-        } else {
+        }
+        if (third >= first && third >= second) {
             result = third;
         }
         return result;
