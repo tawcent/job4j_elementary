3017e8e651a9c1868a43b4473ef2fd93d52fb882
tawcent <rusx745@gmail.com>
2024-05-05 16:35:35 +0400

    8. Стороны прямоугольника. [#137735]

--- src/main/java/ru/job4j/condition/DivideBySix.java
+++ src/main/java/ru/job4j/condition/DivideBySix.java
@@ -5,13 +5,10 @@ public class DivideBySix {
         String result;
         if (num % 2 == 0 && num % 3 == 0) {
             result = "The number divides by 6.";
-
-        } else if (num % 3 == 0 && !(num % 2 == 0)) {
+        } else if (num % 3 == 0) {
             result = "The number divides by 3, but it isn't the even number.";
-
-        } else if (!(num % 3 == 0) && num % 2 == 0) {
+        } else if (num % 2 == 0) {
             result = "The number doesn't divide by 3, but it is the even number.";
-
         } else {
             result = "The number doesn't divide by 3 and it isn't the even number.";
         }
