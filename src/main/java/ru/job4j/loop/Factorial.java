fa02b22a0e2da23f64bbd3e0b09fc19680787ac5
tawcent <rusx745@gmail.com>
2024-05-07 19:53:55 +0400

    8. Стороны прямоугольника. [#137735]

---
+++ src/main/java/ru/job4j/loop/Factorial.java
@@ -0,0 +1,12 @@
+package ru.job4j.loop;
+
+public class Factorial {
+    public static int calculate(int number) {
+        int result = 1;
+        for(int n = 2; n <= number; n++) {
+            result *= n;
+        }
+        return result;
+    }
+}
+
