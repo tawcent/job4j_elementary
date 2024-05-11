6a7e75b2ab61482437966e2c2cb84d26ada8a0fb
tawcent <rusx745@gmail.com>
2024-05-11 15:36:56 +0400

    8. Стороны прямоугольника. [#137735]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -4,8 +4,7 @@ public class Mortgage {
     public static int year(double amount, double salary, double percent) {
         int year = 0;
         while (amount > 0) {
-            double duty = amount + (amount * percent / 100);
-            amount = duty - salary;
+            amount += (amount * percent / 100) - salary;
             year++;
         }
         return year;
