959fd0c90dd55fe37b61851d28760bcc7280eecf
tawcent <rusx745@gmail.com>
2024-05-11 15:29:36 +0400

    8. Стороны прямоугольника. [#137735]

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -3,9 +3,6 @@ package ru.job4j.loop;
 public class CheckPrimeNumber {
     public static boolean check(int number) {
         boolean prime = number > 1;
-        if (number < 2) {
-            return false;
-        }
         for (int i = 2; i < number - 1; i++) {
             if (number % i == 0) {
                 prime = false;
