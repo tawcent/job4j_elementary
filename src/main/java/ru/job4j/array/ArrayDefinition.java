3fc2ecdb4678d64d1fd7f57e6a47c08a3f40c856
tawcent <rusx745@gmail.com>
2024-05-12 18:20:08 +0400

    8. Стороны прямоугольника. [#137735]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,14 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен: " + surnames.length);
         float[] prices = new float[40];
         System.out.println("Размер массива равен: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Dasha";
+        names[1] = "Petr";
+        names[2] = "Elena";
+        names[3] = "Ruslan";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
