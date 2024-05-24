72501249f5daf6cfbfcd2b58682ed8d2a3c3d15d
tawcent <rusx745@gmail.com>
2024-05-24 21:08:01 +0400

    8. Стороны прямоугольника. [#137735]

---
+++ src/main/java/ru/job4j/array/EndsWith.java
@@ -0,0 +1,14 @@
+package ru.job4j.array;
+
+public class EndsWith {
+    public static boolean endsWith(char[] word, char[] postfix) {
+        boolean result = true;
+        for (int index = 0; index < postfix.length; index++) {
+            if (word[word.length - postfix.length + index] != postfix[index]) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
