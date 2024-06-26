45fbf156ad89ae5bd26c7fd2187c0de056aa1546
tawcent <rusx745@gmail.com>
2024-06-26 19:41:43 +0400

    8. Стороны прямоугольника. [#137735]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -3,11 +3,22 @@ package ru.job4j.condition;
 public class Max {
     public static int max(int left, int right) {
         return left > right ? left : right;
+    }
 
+    public static int max(int left, int right, int third) {
+        return max(left,
+                max(right, third)
+        );
+    }
+
+    public static int max(int left, int right, int third, int four) {
+        return max(left, right,
+                max(third, four)
+        );
     }
 
     public static void main(String[] args) {
-       int mess = Max.max(6, 7);
+       int mess = Max.max(6, 7, 8, 1);
        System.out.println(mess);
     }
 }
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -12,4 +12,25 @@ class MaxTest {
         int expected = 2;
         assertThat(result).isEqualTo(expected);
     }
+
+    @Test
+    void whenMax4To2To6Then6() {
+        int left = 4;
+        int right = 2;
+        int third = 6;
+        int result = Max.max(left, right, third);
+        int expected = 6;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenMax5To3To8To10Then2() {
+        int left = 5;
+        int right = 3;
+        int third = 8;
+        int four = 10;
+        int result = Max.max(left, right, third, four);
+        int expected = 10;
+        assertThat(result).isEqualTo(expected);
+    }
 }
