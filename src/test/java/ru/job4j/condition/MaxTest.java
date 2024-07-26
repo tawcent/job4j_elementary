fe55ba8ef8f5581638225424afba4ecd7c42fcfe
tawcent <rusx745@gmail.com>
2024-07-26 22:51:45 +0400

    8. Стороны прямоугольника. [#137735]

--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -1,36 +1,27 @@
 package ru.job4j.condition;
 
-import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
-class MaxTest {
+public class MaxTest {
     @Test
-    void whenMax1To2Then2() {
-        int left = 1;
-        int right = 2;
-        int result = Max.max(left, right);
-        int expected = 2;
-        assertThat(result).isEqualTo(expected);
+    public void whenMaxSecond() {
+        assertThat(Max.max(1, 2), is(2));
     }
 
     @Test
-    void whenMax4To2To6Then6() {
-        int left = 4;
-        int right = 2;
-        int third = 6;
-        int result = Max.max(left, right, third);
-        int expected = 6;
-        assertThat(result).isEqualTo(expected);
+    public void whenMaxFirst() {
+        assertThat(Max.max(3, 2), is(3));
     }
 
     @Test
-    void whenMax5To3To8To10Then2() {
-        int left = 5;
-        int right = 3;
-        int third = 8;
-        int four = 10;
-        int result = Max.max(left, right, third, four);
-        int expected = 10;
-        assertThat(result).isEqualTo(expected);
+    public void whenEquals() {
+        assertThat(Max.max(4, 4), is(4));
+    }
+
+    @Test
+    public void whenNegativeNumber() {
+        assertThat(Max.max(-2, -1), is(-1));
     }
 }
